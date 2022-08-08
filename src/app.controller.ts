e94d44c2880af269ae5d824cfbfb81eee6bb5906
Aman Chaudhary <achoudhary@deqode.com>
2022-08-08 17:12:55 +0530

    chore: integrate app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,7 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Post()
-  getHash(@Req() request: Request): object {
-    return this.appService.getHash(request.body);
-  }
 }
