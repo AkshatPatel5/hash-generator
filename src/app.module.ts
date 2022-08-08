cfb8276d3d6b799ff0b54014c3dc66e187fd64b5
Aman Chaudhary <achoudhary@deqode.com>
2022-08-08 17:14:03 +0530

    feat: configure bull queue with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from '@hapi/joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Hash } from './database/entities/hash.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { BullModule } from '@nestjs/bull';
+import { HashProcessor } from './processors/hash.processor';
+import { BullBoardController } from './bull-board/bull-board.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    BullModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          redis: {
+            host: configService.get('REDIS_HOST'),
+            port: configService.get('REDIS_PORT'),
+          },
+        };
+      },
+      inject: [ConfigService],
+      imports: [ConfigModule],
+    }),
+    BullModule.registerQueue({
+      name: 'upload-hash',
+    }),
+    TypeOrmModule.forFeature([Hash]),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
+  controllers: [AppController, BullBoardController],
+  providers: [AppService, HashProcessor],
 })
 export class AppModule {}
